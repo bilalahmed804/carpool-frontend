b76a85b16fdc50b7bc89a309ed9abdd70193a1f4
Ather Ali Siddiqui <engr.ather.93@gmail.com>
2025-02-25 15:54:48 +0500

    stytling file

---
+++ constant/constant.tsx
@@ -0,0 +1,43 @@
+
+import { StyleSheet } from "react-native";
+
+const globalStyle = StyleSheet.create({
+    heading: {
+        color: "#00008B",
+        fontSize: 20,
+        fontWeight: "bold",
+        fontFamily: "sans-serif",
+    },
+
+    subHeading: {
+        color: "#008000",
+        fontSize: 16,
+        fontWeight: "semibold",
+        fontFamily: "sans-serif",
+    },
+
+    greenButton: {
+        backgroundColor: "#69E29C"
+    },
+    hovergreenButton: {
+        backgroundColor: "#37D67A",
+        elevation: 5,
+    },
+    blueButton: {
+        backgroundColor: "#42B2F2"
+    },
+    hoverblueButton: {
+        backgroundColor: "#0693E9",
+        elevation: 5,
+    },
+
+    textSize: {
+        fontSize: 14
+    },
+    bg: {
+        backgroundColor: "white"
+    }
+
+})
+
+export default globalStyle;
