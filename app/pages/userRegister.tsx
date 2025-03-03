d90044ea642fb1f449e411d42ea82ab8461ea0c2
Bilal Ahmed <134858966+bilalahmed804@users.noreply.github.com>
2025-03-03 10:28:57 +0500

    updata_code

--- app/pages/userRegister.tsx
+++ app/pages/userRegister.tsx
@@ -4,9 +4,9 @@ import globalStyle, { AppRoutes } from "@/constant/constant";
 import { globalContext } from "@/context/globalContext";
 import { Picker } from "@react-native-picker/picker";
 import axios from "axios";
-import { router } from "expo-router";
+import { router, useRouter } from "expo-router";
 import { useContext, useState } from "react";
-import { ScrollView,Text, TextInput, View } from "react-native";
+import { ScrollView, Text, TextInput, View } from "react-native";
 
 export interface form_Data {
   name: string;
@@ -18,149 +18,160 @@ export interface form_Data {
   role?: string
 }
 
-function UserRegister(){
+function UserRegister() {
 
-  const {userprofileImage } = useContext(globalContext)
-  
-     const [formData, setFormData] = useState({
-      name: "",
-      phoneNumber: "",
-      email: "",
-      password: "",
-      address: "",
-      gender: "",
+  const { userprofileImage } = useContext(globalContext)
+  const router = useRouter()
+  const [formData, setFormData] = useState({
+    name: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+    address: "",
+    gender: "",
+    nicNo: ""
+  });
+  const [error, setError] = useState<{ [key: string]: string }>({})
 
-      });
-     const [error , setError] = useState<{ [key: string]: string }>({})
-     
-      const handleChange = (key:any, value:any) => {
-        setFormData({ ...formData, [key]: value });
-      };
-    const handleSubmit =async () => {
-        const newErrors: { [key: string]: string } = {}
-        if(!formData.email) newErrors.email = "email is required";
-        if(!formData.password) newErrors.password = "password is required"
-        if(!formData.name) newErrors.name = "Name is required"; 
-        if(!formData.gender) newErrors.gender = "gender is required";
-        if(!formData.address) newErrors.address = "address is required"
-        if(!formData.phoneNumber) newErrors.phoneNumber = "Phone Number is required";
-        
-        setError(newErrors)
+  const handleChange = (key: any, value: any) => {
+    setFormData({ ...formData, [key]: value });
+  };
+  const handleSubmit = async () => {
+    const newErrors: { [key: string]: string } = {}
+    if (!formData.email) newErrors.email = "email is required";
+    if (!formData.password) newErrors.password = "password is required"
+    if (!formData.name) newErrors.name = "Name is required";
+    if (!formData.gender) newErrors.gender = "gender is required";
+    if (!formData.address) newErrors.address = "address is required"
+    if (!formData.nicNo) newErrors.nicNo = "CNIC is required"
+    if (!formData.phoneNumber) newErrors.phoneNumber = "Phone Number is required";
 
-        const obj = {
-          email: formData.email,
-          password: formData.password,
-          name: formData.name,
-          gender: formData.gender,
-          phoneNumber: formData.phoneNumber,
-          address: formData.address,
-          profileImage: userprofileImage,
-          role: "user",
-        };
-      // let data = { ...formData, role: "user" , profileImage : userprofileImage};
-      console.log("all data",obj);
-      
-      try {
-        const res = await axios.post(AppRoutes.signupUser, obj);
-console.log("res",res);
+    setError(newErrors)
 
-        if (res && res.data) {
-          const data = res.data?.data
-          console.log(data)
-          router.push("/pages/userdashboard")
-          console.log("finlly");
-          
-        }
-      } catch (error) {
-        console.error("error when submiting the data", error);
-      }
+    const obj = {
+      email: formData.email,
+      password: formData.password,
+      name: formData.name,
+      gender: formData.gender,
+      phoneNumber: formData.phoneNumber,
+      nicNo: formData.nicNo,
+      address: formData.address,
+      profileImage: userprofileImage || "",
+      role: "user",
     };
-    return(
-         <ScrollView style={globalStyle.backgroundColor}
-       >
-           <View style={globalStyle.textAlign}>
-                    <Text
-                      style={globalStyle.pageHeading}>
-                      Sign Up
-                    </Text>
-                    </View>
-         <View style={globalStyle.container}>
-              <View style={globalStyle.inputContainer}>
-                
-                <Text style={globalStyle.label}>Name</Text>
-                <TextInput
-                  style={globalStyle.input}
-                  placeholder="Enter your Driver Name"
-                  value={formData.name}
-                  onChangeText={(text) => handleChange('name', text)}
-                />
-                {error.name && <Text>{error.name}</Text>}
-              </View>
-              <View style={globalStyle.inputContainer}>
-                <Text style={globalStyle.label}>Email</Text>
-                <TextInput
-                  style={globalStyle.input}
-                  placeholder="example@gmail.com"
-                  value={formData.email}
-                  onChangeText={(text) => handleChange('email', text)}
-                  keyboardType="email-address"
-                  autoCapitalize="none"
-                  />
-                  {error.email && <Text>{error.email}</Text>}
-              </View>
-              <View style={globalStyle.inputContainer}>
-                  <Text style={globalStyle.label}>Contact</Text>
-                  <TextInput
-                    style={globalStyle.input}
-                    placeholder="Enter your Contact"
-                    value={formData.phoneNumber}
-                    onChangeText={(text) => handleChange('phoneNumber', text.replace(/[^0-9]/g, ''))}
-                    keyboardType="numeric"
-                    maxLength={11}
-                    />
-                    {error.phoneNumber && <Text>{error.phoneNumber}</Text>}
-                </View>
+    // let data = { ...formData, role: "user" , profileImage : userprofileImage};
+    console.log("all data", obj);
+    console.log("userImage", userprofileImage);
+    router.push("/pages/userdashboard")
 
-                <View style={globalStyle.inputContainer}>
-                    <Text style={globalStyle.label}>Password</Text>
-                    <TextInput
-                      style={globalStyle.input}
-                      placeholder="Enter your Password"
-                      value={formData.password}
-                      onChangeText={(text) => handleChange('password', text)}
-                      secureTextEntry
-                      />
-                      {error.password && <Text>{error.password}</Text>}
-                  </View>
-                
-                  <View style={globalStyle.inputContainer}>
-                    <Text style={globalStyle.label}>Address</Text>
-                    <TextInput
-                      style={globalStyle.input}
-                      placeholder="Enter your Address"
-                      value={formData.address}
-                      onChangeText={(text) => handleChange('address', text)}
-                      />
-                      {error.address && <Text>{error.address}</Text>}
-                  </View>
-                  <View style={globalStyle.inputContainer}>
-  <Text style={globalStyle.label}>Select Gender</Text>
-  <Picker
-        selectedValue={formData.gender}
-        onValueChange={(gender) => handleChange('gender', gender)}
-        style={globalStyle.picker}
-        >
-        <Picker.Item label="Select an Gender" value="" />
-        <Picker.Item label="Female" value="female" />
-        <Picker.Item label="Male" value="male" />
-      </Picker>
-      {error.gender && <Text>{error.gender}</Text>}
+    try {
+      const res = await axios.post(AppRoutes.signupUser, obj);
+      const data = res.data?.data
+    if(data) return router.push("/pages/userdashboard")
+        console.log("finlly");
+    } catch (error:any) {
+      console.error("error when submiting the data", error.error.response?.data || error.message);
+    }
+  };
+  return (
+    <ScrollView style={globalStyle.backgroundColor}
+    >
+      <View style={globalStyle.textAlign}>
+        <Text
+          style={globalStyle.pageHeading}>
+          Sign Up
+        </Text>
+      </View>
+      <View style={globalStyle.container}>
+        <View style={globalStyle.inputContainer}>
+
+          <Text style={globalStyle.label}>Name</Text>
+          <TextInput
+            style={globalStyle.input}
+            placeholder="Enter your Driver Name"
+            value={formData.name}
+            onChangeText={(text) => handleChange('name', text)}
+          />
+          {error.name && <Text>{error.name}</Text>}
+        </View>
+        <View style={globalStyle.inputContainer}>
+          <Text style={globalStyle.label}>Email</Text>
+          <TextInput
+            style={globalStyle.input}
+            placeholder="example@gmail.com"
+            value={formData.email}
+            onChangeText={(text) => handleChange('email', text)}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          {error.email && <Text>{error.email}</Text>}
+        </View>
+        <View style={globalStyle.inputContainer}>
+          <Text style={globalStyle.label}>Contact</Text>
+          <TextInput
+            style={globalStyle.input}
+            placeholder="Enter your Contact"
+            value={formData.phoneNumber}
+            onChangeText={(text) => handleChange('phoneNumber', text.replace(/[^0-9]/g, ''))}
+            keyboardType="numeric"
+            // maxLength={11}
+          />
+          {error.phoneNumber && <Text>{error.phoneNumber}</Text>}
+        </View>
+
+        <View style={globalStyle.inputContainer}>
+          <Text style={globalStyle.label}>Password</Text>
+          <TextInput
+            style={globalStyle.input}
+            placeholder="Enter your Password"
+            value={formData.password}
+            onChangeText={(text) => handleChange('password', text)}
+            secureTextEntry
+          />
+          {error.password && <Text>{error.password}</Text>}
+        </View>
+
+        <View style={globalStyle.inputContainer}>
+          <Text style={globalStyle.label}>CNIC</Text>
+          <TextInput
+            style={globalStyle.input}
+            placeholder="Enter your CNIC (without dashes)"
+            value={formData.nicNo}
+            onChangeText={(text) => handleChange('nicNo', text.replace(/[^0-9]/g, ''))}
+            keyboardType="numeric"
+            // maxLength={13}
+          />
+          {error.nicNo && <Text>{error.nicNo}</Text>}
+        </View>
+
+        <View style={globalStyle.inputContainer}>
+          <Text style={globalStyle.label}>Address</Text>
+          <TextInput
+            style={globalStyle.input}
+            placeholder="Enter your Address"
+            value={formData.address}
+            onChangeText={(text) => handleChange('address', text)}
+          />
+          {error.address && <Text>{error.address}</Text>}
+        </View>
+        <View style={globalStyle.inputContainer}>
+          <Text style={globalStyle.label}>Select Gender</Text>
+          <Picker
+            selectedValue={formData.gender}
+            onValueChange={(gender) => handleChange('gender', gender)}
+            style={globalStyle.picker}
+          >
+            <Picker.Item label="Select an Gender" value="" />
+            <Picker.Item label="Female" value="female" />
+            <Picker.Item label="Male" value="male" />
+          </Picker>
+          {error.gender && <Text>{error.gender}</Text>}
 
         </View>
-        <UserImage/>
-              <BlueButton text="Submit" onPress={handleSubmit}/>    
-        </View>
-        </ScrollView>
-    )
+        <UserImage />
+        <BlueButton text="Submit" onPress={handleSubmit} />
+      </View>
+    </ScrollView>
+  )
 };
 export default UserRegister;
--- app/pages/userdashboard.tsx
+++ app/pages/userdashboard.tsx
@@ -2,92 +2,18 @@ import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { Ionicons } from "@expo/vector-icons"; 
+import Map from "@/components/Map";
 
 const UserDashboard = () => {
   const [initialLocation, setInitialLocation] = useState("");
   const [destination, setDestination] = useState("");
   return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={StyleSheet.absoluteFillObject}
-        initialRegion={{
-          latitude: 24.8607,
-          longitude: 67.0011,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
-        }}
-      />
-      <View style={styles.navbar}>
-        <Ionicons name="menu" size={30} color="#007BFF" />
-      </View>
+    <View>
 
-      <View style={styles.rideContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter pickup location"
-          placeholderTextColor="gray"
-          value={initialLocation}
-          onChangeText={setInitialLocation}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Enter destination"
-          placeholderTextColor="gray"
-          value={destination}
-          onChangeText={setDestination}
-        />
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Search Ride</Text>
-        </TouchableOpacity>
-      </View>
+    <Map/>
     </View>
   );
 };
 
 export default UserDashboard;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  navbar: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    backgroundColor: "transparent",
-    padding: 10,
-    paddingTop: 20,
-    color:"blue",
-  },
-
-  rideContainer: {
-    position: "absolute",
-   borderColor:"#007BFF",
-   borderWidth:2,
-    bottom: 10,
-    left: 20,
-    right: 20,
-    backgroundColor: "lightgray",
-    padding: 10,
-    borderRadius: 10,
-  },
-  input: {
-    backgroundColor: "white",
-    color: "black",
-    padding: 8,
-    borderRadius: 5,
-    marginBottom: 10,
-  },
-  button: {
-    backgroundColor: "#007BFF",
-    padding: 12,
-    borderRadius: 5,
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
