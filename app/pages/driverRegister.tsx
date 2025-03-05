1b9bd8e529d762c0ad4529001aabdde7f2d68b79
Bilal Ahmed <134858966+bilalahmed804@users.noreply.github.com>
2025-03-05 10:46:31 +0500

    driverupdate

--- app/pages/driverRegister.tsx
+++ app/pages/driverRegister.tsx
@@ -66,7 +66,6 @@ function DriverRegister(){
     };
     console.log("obj", obj);
     
-    router.push("/pages/driverdashboard")
     try {
       const res = await axios.post(AppRoutes.signupRider, obj);
       if (res && res.data) {
