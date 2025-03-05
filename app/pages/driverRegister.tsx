b4fcc9009818258a14396c21f2e7f99559d5c347
Bilal Ahmed <134858966+bilalahmed804@users.noreply.github.com>
2025-03-05 10:45:30 +0500

    driverchanges

--- app/pages/driverRegister.tsx
+++ app/pages/driverRegister.tsx
@@ -65,8 +65,6 @@ function DriverRegister(){
       role: "driver",
     };
     console.log("obj", obj);
-    
-    router.push("/pages/driverdashboard")
     try {
       const res = await axios.post(AppRoutes.signupRider, obj);
       if (res && res.data) {
